58dbdc0b96873bf027daddc066281ef2975a9a72
Pikatchu99 <yemalin.agbangla@epitech.eu>
2021-09-15 09:53:46 +0100

    delete config.i18n.default_locale

--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,10 @@ module InstagramApp
   class Application < Rails::Application
     config.exceptions_app = self.routes
     config.load_defaults 5.2
-    config.time_zone = 'West Central Africa'
-    config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
-    config.i18n.default_locale = :en
+   # config.time_zone = 'West Central Africa'
+  #  config.active_record.default_timezone = :local
+ #   config.i18n.default_locale = :ja
+#    config.i18n.default_locale = :en
 
     config.generators do |g|
       g.assets false
